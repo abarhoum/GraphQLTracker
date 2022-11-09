e4da02e8706640f6eb93e8892548dafdf7395162
Ayman Barhoum <17288223+abarhoum@users.noreply.github.com>
2022-11-09 17:27:38 +0400

    remove comment

--- src/GraphQLTracker/GraphQLTracker/Pipelines/GraphQLWarningMessage.cs
+++ src/GraphQLTracker/GraphQLTracker/Pipelines/GraphQLWarningMessage.cs
@@ -10,11 +10,7 @@ namespace GraphQLTracker.Pipelines
 {
     public class GraphQLWarningMessage
     {
-        /// <summary>
-        ///  For sake of demo, I am reading the date from the item itself and displaying in the warning bar. 
-        ///  for dansbank, since they are going to save all the requested items in database table, they can use this pipeline to to check if arg.Item exists the table  ? if yes then fill the requested data field, and populate the warning message.
-        /// </summary>
-        /// <param name="args"></param>
+
         public void Process(GetContentEditorWarningsArgs args)
         {
           
